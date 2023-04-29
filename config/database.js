b3ac8fff04705666750bdd1f00073b9180c51968
valerythedev <valgonzr@gmail.com>
2023-04-29 11:17:58 -0400

    working tree

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://valgonzr:ennIuqqxuHx0NikV@demon.5xoynxc.mongodb.net/test?retryWrites=true&w=majority', 
+    'dbName': 'savage-auth'
+};
